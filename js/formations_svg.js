f4cd29be9533ba6319c16c4d235295f4c45e80ec
rishizsinha <rishi.z.sinha@gmail.com>
2015-12-25 06:59:43 -0800

    stage 1 js functions outlined

---
+++ js/formations_svg.js
@@ -0,0 +1,19 @@
+// Stage Dimensions and Sizings
+var stageHeight = 400;
+var stageWidth = 1000; 
+var personRadius = 20;
+
+$("#stageResize").click( function() {
+	// assume 1ft=50px
+	// re-assign stageHeight and stageWidth vars
+
+	// change personRadius
+
+	// animate to have size change reflect circle size change
+
+});
+
+$("#addPerson").click( function() {
+	// add circle to "#people" svg group element
+	
+});
